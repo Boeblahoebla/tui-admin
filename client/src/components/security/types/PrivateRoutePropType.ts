11b98fe098d5cefe404ea7e9c739fb151fc0cbbd
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-12-20 00:10:56 +0100

    removed a redundant file

--- client/src/components/security/types/PrivateRoutePropType.ts
+++ client/src/components/security/types/PrivateRoutePropType.ts
@@ -10,7 +10,6 @@ import { RouteProps } from "react-router";
 
 // PrivateRoutePropType
 export interface PrivateRoutePropType extends RouteProps {
-    // tslint:disable-next-line:no-any
     component: any;
     auth: reduxFullAuthState;
 }
