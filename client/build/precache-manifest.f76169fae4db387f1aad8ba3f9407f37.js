6dcbfdec97d4858e1cd9e86fccccbd21239ee19d
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-12-22 14:49:32 +0100

    created own build

---
+++ client/build/precache-manifest.f76169fae4db387f1aad8ba3f9407f37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1248ffead494c3b2caf870c0cd297cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1fd39cbda28d24e0ac",
+    "url": "/static/css/main.68011451.chunk.css"
+  },
+  {
+    "revision": "9d0d8b1535353fd16ea7",
+    "url": "/static/js/2.9c92283f.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.9c92283f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd1fd39cbda28d24e0ac",
+    "url": "/static/js/main.fe8dad04.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "f45c127c8b5351857c3b117549bc0540",
+    "url": "/static/media/TUI_logo_rgb.f45c127c.png"
+  },
+  {
+    "revision": "16f448183fc5953c297044062590e04e",
+    "url": "/static/media/spinner.16f44818.gif"
+  }
+]);
